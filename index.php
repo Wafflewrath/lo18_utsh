0635cc83100359af82deb504fa95a40e23d5fecd
Zicouto <c.bras12@gmail.com>
2014-04-10 11:58:49 +0200

    Header Include

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include('header.php') ?>
+	<div class="content row">
+		<div class="col-lg-3 grptuile">
+			<div class="tuile">
+				Qui sommes-nous?
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			</div>
+			<div class="tuile">
+				Accès au projet et calendrier
+				<img src="./style/images/calendrier-web-ergonomie.gif" />
+			</div>
+		</div>
+		<div class="col-lg-5 grptuile">
+			<div class="tuile"><h1>News</h1></div>
+		</div>
+		<div class="col-lg-4 grptuile">
+			<div class="tuile">Connexion</div>
+			<div class="tuile">Forums</div>
+			<div class="tuile">liens ressources</div>
+		</div>
+	</div>
+<?php include('footer.php') ?>
