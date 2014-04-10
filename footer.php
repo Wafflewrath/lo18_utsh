e8440aaa17bca026c5e3f6ba9b6fab4632f2972e
Zicouto <c.bras12@gmail.com>
2014-04-10 11:31:26 +0200

    footer
    footer et logo

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="row">
+	<div class="col-lg-4">
+		<div class="contact col-lg-6">Nous contacter :</div>
+		<div class="mail col-lg-6">
+			<ul class="mail">
+				<li>charles.lenay@hds.utc.fr</li>
+	        	<li>charles.lenay@hds.utc.fr</li>
+	        	<li>Charles.lenay@hds.utc.fr</li>
+	        </ul>
+	    </div>
+	</div>
+	<div class="col-lg-1 imgfoot">
+	</div>
+	<div class="col-lg-3">
+	</div>
+	<div class="col-lg-1 imgfoot">
+	</div>
+	<div class="col-lg-3">
+	</div>
+</footer>
