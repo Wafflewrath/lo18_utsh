9c1746ca94c3aecc2c4c7c2eefdbb3c31ee8715e
Wafflewrath <quentinlegrand53@aol.com>
2014-04-10 10:49:57 +0200

    First commit
    Debut de branche master

---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="style/header.css" />
+	<script type="text/JavaScript" src="js/jquery.js"></script>
+	<script type="text/JavaScript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header>
+		<div class="degrade col-lg-12">
+			<div class="logo"></div>
+		</div>
+		
+		<div class="image_en_tete col-lg-12"></div>
+	</header>
+</body>
