9a1015dfe637bc496d936cd76ddddba9919bd346
Wafflewrath <quentinlegrand53@aol.com>
2014-04-13 20:36:24 +0200

    CSS + qui sommes nous
    rajout hover, changement sur les titres, et les tuiles
    Ajout page qui sommes nous

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <<<<<<< HEAD
 <?php include('header.php') ?>
 	<div class="content row">
+	
 		<div class="col-lg-3 grptuile">
-			<div class="tuile">
-				Qui sommes-nous?
+			<div class="tuile" onclick="window.location.href='qui_sommes_nous.php';">
+				<div class="tuiletitle">Qui sommes-nous?</div>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
 			</div>
+			
 			<div class="tuile">
-				Accès au projet et calendrier
+				<div class="tuiletitle">Accès au projet et calendrier</div>
 				<img src="./style/images/calendrier-web-ergonomie.gif" />
 			</div>
 		</div>
+		
 		<div class="col-lg-5 grptuile">
-			<div class="tuile"><h1>News</h1></div>
+			<div class="tuile"><h1 id="newstitle">News</h1></div>
 		</div>
+		
 		<div class="col-lg-4 grptuile">
-			<div class="tuile">Connexion</div>
-			<div class="tuile">Forums</div>
-			<div class="tuile">liens ressources</div>
+			<div class="tuile tuiletitle">Connexion</div>
+			<div id="forumstuile" class="tuile">
+				<div class="tuiletitle">Forums</div>
+				<div class="forumpost">
+				<div class="forumsujet">Sujet 1</div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.
+				</div>
+				<div class="forumpost">
+				<div class="forumsujet">Sujet 1</div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.
+				</div>
+			</div>
+			<div class="tuile tuiletitle">liens ressources</div>
 		</div>
+		
 	</div>
 <?php include('footer.php') ?>
 =======
