f896f30d6f9a05013a4b929a8f53096a18a8407f
kahn-aar <nicolas.martinr@gmail.com>
2014-04-15 17:49:12 +0200

    Mise en place de la page présentation
    Ajout de la page présentation et ajout de barre sous les titres

---
+++ presentation.php
@@ -0,0 +1,40 @@
+<?php include('header.php') ?>
+	<div class="content row">
+		<ol class="breadcrumb">
+		  <li><a href="index.php">Accueil</a></li>
+		  <li><a href="qui_sommes_nous.php">Qui sommes nous</a></li>
+		  <li class="active">Présentation</li>
+		</ol>
+
+		<div class="tuile_container col-lg-8">
+			<div class="lineHeader">
+				<h2>Présentation</h2>
+			</div>
+			<br />
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.</p>
+			
+		</div>
+		
+		<div class="col-lg-4 grptuile">
+			<div class="tuile tuiletitle">Connexion</div>
+			<div id="forumstuile" class="tuile">
+				<div class="tuiletitle">Forums</div>
+				<div class="forumpost">
+					<div class="forumsujet">Sujet 1</div>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.
+				</div>
+				<div class="forumpost">
+					<div class="forumsujet">Sujet 1</div>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros.
+				</div>
+			</div>
+		</div>
+		
+	</div>	
+
+<?php include('footer.php') ?>
--- qui_sommes_nous.php
+++ qui_sommes_nous.php
@@ -1,14 +1,14 @@
 <?php include('header.php') ?>
 	<div class="content row">
 		<ol class="breadcrumb">
-		  <li><a href="#">Accueil</a></li>
+		  <li><a href="index.php">Accueil</a></li>
 		  <li class="active">Qui sommes nous</li>
 		</ol>
 
 		<div class="tuile_container col-lg-8">
 			<div class="row">
 				<div class="col-lg-6 grptuile">
-					<div class="tuile max_250">
+					<div class="tuile max_250" onclick="window.location.href='presentation.php';">
 						<div class="tuiletitle">Qui sommes-nous ?</div>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante TEXTE DE PRESENTATION ipsum primis in faucibus. Nam vitae dapibus orci. Sed vel aliquam mi, eu mattis eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor.
 						</p>
