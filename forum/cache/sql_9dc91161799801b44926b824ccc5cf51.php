36ff35c9d2e7ea842b7b5e89cc76baac75d15354
Zicouto <c.bras12@gmail.com>
2014-04-15 18:40:46 +0200

    modif css
    css header

--- forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
+++ forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1397578670
+1397583194
 SELECT ban_ip, ban_userid, ban_email, ban_exclude, ban_give_reason, ban_end FROM phpbb_banlist WHERE ban_email = '' AND (ban_userid = 1 OR ban_ip <> '')
 6
 a:0:{}
