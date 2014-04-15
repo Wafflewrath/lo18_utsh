efcb964f7dc716963f234ebdccc0dc86dd6ecd28
Zicouto <c.bras12@gmail.com>
2014-04-15 19:17:10 +0200

    cache

--- forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
+++ forum/cache/sql_9dc91161799801b44926b824ccc5cf51.php
@@ -1,17 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-1397583642
-=======
-<<<<<<< HEAD
-1397583194
-=======
-1397582242
->>>>>>> dce6eafdc3cdd86753003b2c8f565646fce4d5d2
->>>>>>> 0d361e8437f1750131e3c4126e596a349dc8ff27
-=======
-1397584573
->>>>>>> 41d6e89ba8736d7315e51bed37948a16ff0c3f04
+1397585187
 SELECT ban_ip, ban_userid, ban_email, ban_exclude, ban_give_reason, ban_end FROM phpbb_banlist WHERE ban_email = '' AND (ban_userid = 1 OR ban_ip <> '')
 6
 a:0:{}
