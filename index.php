dc2e4811dad1dc039f44f7b34b633ec565b7d877
Wafflewrath <quentinlegrand53@aol.com>
2014-04-15 18:31:05 +0200

    index changes

--- index.php
+++ index.php
@@ -18,7 +18,25 @@
 		</div>
 		
 		<div class="col-lg-4 grptuile">
-			<div class="tuile tuiletitle">Connexion</div>
+				<?php
+					if($user->data['is_registered'])
+					{
+						// j’utilise ici les fonctions interne à phpBB3 pour interroger la base de donnée sur le nombre de nouveaux message.
+						$nb_new_msg =$db->sql_query("SELECT COUNT(msg_id) AS nb FROM phpbb_privmsgs_to WHERE user_id='".$user->data['user_id']."' AND pm_new=1 LIMIT 1");
+						$nb_new_msg =$db->sql_fetchrow($nb_new_msg);
+						$javascript_to_add = "window.location.href='".$phpbb_root_path."ucp.php?mode=logout&redir=1&sid=".$user->session_id."'";
+						echo '<div class="tuile tuiletitle" onclick="'.$javascript_to_add.'">';
+						echo 'Déconnexion';
+						echo '</div>';
+					}
+					else
+					{
+						$javascript_to_add = "window.location.href='./login_register.php'";
+						echo '<div class="tuile tuiletitle" onclick="'.$javascript_to_add.'">';
+						echo 'Connexion / Inscription';
+						echo '</div>';
+					}
+				?>
 			<div id="forumstuile" class="tuile">
 				<div class="tuiletitle">Forums</div>
 				<div class="forumpost">
@@ -33,23 +51,4 @@
 			<div class="tuile tuiletitle">Liens / Ressources</div>
 		</div>	
 	</div>
-<?php
-	echo 'hello world';
-	if(!$user->data['is_bot']) 
-	{ // Si l’utilisateur n’est pas un bot (Google, Yahoo, …)
-		if(!$user->data['is_registered'])
-		{ // Si l’utilisateur n’est pas encore logué
-			echo '<a href="'.$phpbb_root_path.'ucp.php?mode=login&redir=1">Connexion</a> |
-			 <a href="'.$phpbb_root_path.'ucp.php?mode=register">Inscription</a>';
-		}
-		else
-		{ // Autrement, si l’utilisateur est logué
-			// j’utilise ici les fonctions interne à phpBB3 pour interroger la base de donnée sur le nombre de nouveaux message.
-			$nb_new_msg =$db->sql_query("SELECT COUNT(msg_id) AS nb FROM phpbb_privmsgs_to WHERE user_id='".$user->data['user_id']."' AND pm_new=1 LIMIT 1");
-			$nb_new_msg =$db->sql_fetchrow($nb_new_msg);
-			echo '<a href="'.$phpbb_root_path.'ucp.php?mode=logout&redir=1&sid='.$user->session_id.'">Déconnexion [ '.$user->data['username'].' ]</a> |
-			 <a href="'.$phpbb_root_path.'ucp.php?i=pm&folder=inbox">Vous avez <strong>'.$nb_new_msg['nb'].'</strong> nouveau message</a>';
-		}
-	}
-?>
 <?php include('footer.php') ?>
