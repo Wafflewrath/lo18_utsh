6f2cfd31efd2158f4ff4bb76d7494db2410b6cd2
Zicouto <c.bras12@gmail.com>
2014-04-15 18:57:42 +0200

    Merge branch 'master' of https://github.com/Wafflewrath/lo18_utsh
    Conflicts:
    	forum/cache/sql_021151d1a377d62dbfaa89a4d1acc716.php
    	forum/cache/sql_b152bea2b86cc301f01da4d7125e73ba.php
    	forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php

--- forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,9 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-1397580990
-=======
-1397581272
->>>>>>> 091b8885de934b5f6c568824223c067253108412
+1397581574
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
