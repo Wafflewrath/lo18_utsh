1ddff8c40bbaa7eb4f05119fefcf9d0aa4f4da7d
Wafflewrath <quentinlegrand53@aol.com>
2014-04-29 14:41:09 +0200

    minor change

--- include/news_adapter.class.php
+++ include/news_adapter.class.php
@@ -41,26 +41,5 @@ class News_adapter
 			echo "ERREUR N° 1.103 - class News_adapter : the class ".get_class($this->news_class)." was not found";
 		}
 	}
-	
-
-	/*
-	<div class="tuile_container col-lg-8">
-			<div class="lineHeader">
-				<h2>Compte rendu de réunion</h2>
-			</div>
-			<div class="news_date">
-				23/04/2014 14h15
-			</div>
-			<br />
-		
-			<div class="news_resume">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum nulla nisl, non mattis magna commodo vel. Maecenas a enim nec ante tincidunt convallis non vitae velit. Phasellus faucibus, lorem id accumsan facilisis, lectus lectus accumsan sapien, ut dapibus nunc risus id tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-			</div>
-			
-			
-			
-		</div> */	
 }
 ?>
