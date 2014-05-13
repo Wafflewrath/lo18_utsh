8e1328180965649869b5998a1a855f6d813e6ea0
kahn-aar <nicolas.martinr@gmail.com>
2014-05-13 19:28:11 +0200

    Modification du footer
    Ahah je suis meilleur ! pas de cache

--- footer.php
+++ footer.php
@@ -1,20 +1,28 @@
 
 <footer class="row footer">
-	<div class="col-lg-5">
-		<div class="contact col-lg-4 tuile_contacttitle tuile_nohover">Nous contacter :</div>
-			<div class="mail col-lg-8">
-				<div class="tuile_contact">charles.lenay@utc.fr</div>
-	        	<div class="tuile_contact">charles.lenay@utc.fr</div>
-	        	<div class="tuile_contact">Charles.lenay@utc.fr</div>
-	        </div>
-	    </div>
-	</div>
+	<div class="contact col-lg-1">Nous contacter :</div>
+	<div class="col-lg-3">
+		<div>
+			<div class="utc_logo" onclick="window.location.href='http://www.utc.fr';"></div>
+			<div class="contact_"><a href="mailto:charles.lenay@utc.fr">Charles Lenay</a></div>
+		</div>
+		<div>
+			<div class="utt_logo" onclick="window.location.href='http://www.utt.fr';"></div>
+			<div class="contact_"><a href="mailto:charles.lenay@utc.fr">Charles Lenay</a></div>
+		</div>
+    </div>
+
 	<div class="col-lg-1 imgfoot">
 	</div>
+
 	<div class="col-lg-3">
-	</div>
-	<div class="col-lg-1 imgfoot">
-	</div>
-	<div class="col-lg-3">
-	</div>
+		<div>
+			<div class="utbm_logo" onclick="window.location.href='http://www.utbm.fr';"></div>
+			<div class="contact_"><a href="mailto:charles.lenay@utc.fr">Charles Lenay</a></div>
+		</div>
+		<div>
+			<div class="lasalle_logo" onclick="window.location.href='http://www.lasalle-beauvais.fr/';"></div>
+			<div class="contact_"><a href="mailto:charles.lenay@utc.fr">Charles Lenay</a></div>
+		</div>
+    </div>
 </footer>
