e1ff6ce299084900544ba63c5659a558a029d59a
kahn-aar <nicolas.martinr@gmail.com>
2014-06-04 11:59:53 +0200

    Epic commit

--- calendar.php
+++ calendar.php
@@ -1,5 +1,5 @@
 			<div class=<?php echo '"tuilecalendar'; if(! $user->data['is_registered']){ echo " desactivate"; } echo '"'; ?> >
-					<div class=<?php echo '"tuiletitle'; if(! $user->data['is_registered']){ echo " alone"; } echo '"'; ?> >Accès au projet et calendrier</div>
+					<div class=<?php echo '"tuiletitle'; if(! $user->data['is_registered']){ echo " alone"; } echo '"'; ?> >Calendrier</div>
 					<div id="my-calendar"></div>
 					<?php $Privilege_manager->execif_Admin("echo '<a href=\"edit_reu.php\" class=\"createnews_link\">Créer un event</a>';") ?>
 			</div>
