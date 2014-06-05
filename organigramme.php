0f72ef7669da63dfdbd40ddbc032ecd36313b6fc
kahn-aar <nicolas.martinr@gmail.com>
2014-06-05 08:58:05 +0200

    Ajout de l'organigramme

---
+++ organigramme.php
@@ -0,0 +1,18 @@
+<?php include('header.php') ?>
+<div class="content row">
+	<ol class="breadcrumb">
+	    <li><a href="index.php">Accueil</a></li>
+	    <li><a href="présentation.php">Présentation</a></li>
+		  <li class="active">Organigramme</li>
+	</ol>
+	<div class="tuile_container col-lg-8">
+		<div class="lineHeader">
+			<h2>Organigramme du GIS</h2>
+		</div>
+		<img src="images/organigramme.png"></img>
+	</div>
+
+
+<?php include('left_colomn_presentation.php'); ?>
+</div>
+<?php include('footer.php') ?>
